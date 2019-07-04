7badb44b03d02a49ca54c5a835e70fa6d0dae3b6
kierepkae <kierepkaedyta@gmail.com>
2019-07-04 09:41:08 +0100

    created Hash and puts result

---
+++ hashes.rb
@@ -0,0 +1,10 @@
+curse = 'dang'
+
+beratings = {
+  "damn" => "That's a curse word! How dare you?!",
+  "dang" => "That's a less bad curse word!!, Still, how dare you ?!",
+  "darn" => "Hmm, I'm mildly offended but I'll survive. Watch your language!",
+  "durn" => " Ahh, that's good Southern swearing, that is!"
+}
+
+puts beratings[curse]
