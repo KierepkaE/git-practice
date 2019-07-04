ae85c125438e47975640673342d8ff883704f289
kierepkae <kierepkaedyta@gmail.com>
2019-07-04 09:53:42 +0100

    created dictionar using hash

---
+++ hash-dictonary.rb
@@ -0,0 +1,10 @@
+words = {
+  "bear" => "a creature that fished in the river for salmon.",
+  "river" => "a body of water that contains salmon and sometimes bears.",
+  "salmon" => "a fish, sometimes in a river, sometimes in a bear and sometimes in both."
+ }
+
+ puts "Word: "
+ word = gets.chomp
+
+ puts words[word]
