4d30c2931f7d66465734ada5fa5a4730e19f6ff8
kierepkae <kierepkaedyta@gmail.com>
2019-07-07 13:30:46 +0100

    created Email class using Hashes

---
+++ Email2.rb
@@ -0,0 +1,24 @@
+class Email
+  def initialize(subject,headers)
+    @subject = subject
+    @headers = headers
+  end
+  def subject
+    @subject
+  end
+
+  def date
+    @headers[:date]
+  end
+
+  def from
+    @headers[:from]
+  end
+
+end
+email = Email.new("Keep on learning with Makers!", { :date => "2019-07-07", :from => "Coach" })
+
+
+puts "Date:    #{email.date}"
+puts "From:    #{email.from}"
+puts "Subject: #{email.subject}"
